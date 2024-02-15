42bf8569e759670bd0d3f67ecc5b7e6039dd7a28
João Micheletti <158852869+FormVenda@users.noreply.github.com>
2024-02-15 20:41:20 -0300

    update
    update

---
+++ cantinho_da_bi/scriptcarrocel.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+
+function moveSlide(n) {
+  const slides = document.querySelectorAll('.carousel-inner img');
+  slideIndex += n;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  const translateValue = -slideIndex * 100;
+  document.querySelector('.carousel-inner').style.transform = `translateX(${translateValue}%)`;
+}
