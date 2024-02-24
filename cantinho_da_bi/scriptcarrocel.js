cb0b7112c2edb9962c31894034f915d990f88316
João Micheletti <158852869+FormVenda@users.noreply.github.com>
2024-02-24 18:50:37 -0300

    falta os valores e finish
    falta os valores e finish

--- cantinho_da_bi/scriptcarrocel.js
+++ cantinho_da_bi/scriptcarrocel.js
@@ -1,10 +1,21 @@
+var elemento = document.querySelector('#produtos');
+var displayAtual = window.getComputedStyle(elemento).getPropertyValue('width');
+var reple = displayAtual.replace("px","");
+var valorInt = parseInt(reple);
 let slideIndex = 0;
+var num = 0;
+if(valorInt > 800){
+  num = 6;
+} else if(valorInt < 480){
+  num = 12;
+};
 
 function moveSlide(n) {
   const slides = document.querySelectorAll('.carousel-inner img');
   slideIndex += n;
-  if (slideIndex > 7 || slideIndex < 0) {
-    slideIndex = 0;   
+  if (slideIndex > num || slideIndex < 0) {
+    slideIndex = 0;
+    console.log(valorInt);
   }
   if (slideIndex < 0) {
     slideIndex = slides.length - 1;
